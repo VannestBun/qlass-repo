747861ddb4f61484471d9a0c92f5cfcbfd089deb
VannestBun <114324735+VannestBun@users.noreply.github.com>
2023-10-20 14:55:17 +1100

    add files

---
+++ script.js
@@ -0,0 +1,11 @@
+const saveBtn = document.getElementById("save-btn")
+
+saveBtn.addEventListener("click", function() {
+    let userEmail = document.getElementById("email")
+    userEmailValue = userEmail.value
+    console.log(userEmailValue)
+    userEmail.value = ''
+
+})
+
+
