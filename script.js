38a76893dcd775a920fff5a73ac35f699e9cd038
VannestBun <114324735+VannestBun@users.noreply.github.com>
2023-11-25 18:23:10 +0700

    carousel added

--- script.js
+++ script.js
@@ -8,5 +8,27 @@ saveBtn.addEventListener("click", function() {
 
 })
 
+const slides = document.querySelector('.slides');
+const slideWidth = document.querySelector('.slide').offsetWidth;
+let currentIndex = 0;
+
+function updateCarousel() {
+  const offset = -currentIndex * slideWidth+ 'px';
+  slides.style.transform = 'translateX(' + offset + ')';
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.children.length;
+  updateCarousel();
+  document.getElementById('next-btn').style.display = 'none'
+  document.getElementById('prev-btn').style.display = 'block'
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + slides.children.length) % slides.children.length;
+  updateCarousel();
+  document.getElementById('next-btn').style.display = 'block'
+  document.getElementById('prev-btn').style.display = 'none'
+}
 
 
